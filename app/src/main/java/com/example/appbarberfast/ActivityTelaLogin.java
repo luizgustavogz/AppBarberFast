fbb41532475d97591bd037215a5ca0afc841b234
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2022-11-21 16:36:21 -0300

    Pequenas mudanças - Tela login

--- app/src/main/java/com/example/appbarberfast/ActivityTelaLogin.java
+++ app/src/main/java/com/example/appbarberfast/ActivityTelaLogin.java
@@ -2,7 +2,9 @@ package com.example.appbarberfast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -10,8 +12,6 @@ import android.widget.TextView;
 
 public class ActivityTelaLogin extends AppCompatActivity {
 
-    ImageView logo_barberfast;
-    EditText email, senha;
     Button btnEntrar;
     TextView txtCadastro;
 
@@ -21,10 +21,34 @@ public class ActivityTelaLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        logo_barberfast = (ImageView) findViewById(R.id.logo_barberfast);
-        email = (EditText) findViewById(R.id.email);
-        senha = (EditText) findViewById(R.id.senha);
         btnEntrar = (Button) findViewById(R.id.btnEntrar);
         txtCadastro = (TextView) findViewById(R.id.txtCadastro);
+
+        //abrirTelaCadastro();
+        //abrirTelaMenu();
     }
+
+    /*
+    public void abrirTelaCadastro(){
+        txtCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaCadastro = new Intent(getApplicationContext(), ActivityTelaCadastro.class);
+                startActivity(telaCadastro);
+            }
+        });
+    }
+    */
+
+    /*
+    public void abrirTelaMenu(){
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaMenu = new Intent(getApplicationContext(), ActivityTelaMenu.class);
+                startActivity(telaMenu);
+            }
+        });
+    }
+    */
 }
